45c9b9b653957d9136397b6591e6c2a01bb8168f
Michael Davids <michaeldavids@michaels-mbp.home>
2023-10-23 21:01:16 +0100

    completed out of strings to console, variables total, average change and total months operations

--- index.js
+++ index.js
@@ -86,3 +86,62 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+/* The total number of months included in the dataset.
+
+* The net total amount of Profit/Losses over the entire period.
+
+* The average of the **changes** in Profit/Losses over the entire period.
+  * You will need to track what the total change in Profit/Losses are from month to month and then find the average.
+  * (`Total/(Number of months - 1)`)
+
+* The greatest increase in Profit/Losses (date and amount) over the entire period.
+
+* The greatest decrease in Profit/Losses (date and amount) over the entire period.
+
+When you open your code in the browser your resulting analysis should look similar to the following:
+
+  ```text
+  Financial Analysis 
+  ----------------
+  Total Months: 86
+  Total: $38382578
+  Average Change: -2315.12
+  Greatest Increase in Profits/Losses: Feb-2012 ($1926159)
+  Greatest Decrease in Profits/Losses: Sep-2013 ($-2196167)
+  ```
+
+Your final code should print the analysis to the console.
+
+variables:
+1. totalMonths - count of months - log to console
+2. total - addition of all profit/loss field - log to console
+3. averageChange - total/(totalMonths -1) - log to console
+4. greatestProfitIncrease - loops through parent (Finances)array to find highest profit number and console log its corresponding child array (showing month & profit/loss value)
+5. greatestDecrease - loops through parent (Finances) array and find lowest profit number and console log its corresponding child array (showing month & profit/loss value)
+
+
+
+*/
+console.log('Financial Analysis');
+console.log('-------------------');
+let totalMonths= finances.length;
+console.log('Total Months: ' + totalMonths);
+
+
+var total = 0
+for(var i=0; i<finances.length; i++){
+  total += finances[i][1];
+}
+console.log('Total: ' + total);
+
+
+let averageChange = total/(totalMonths-1);
+console.log('Average Change: ' + averageChange)
+
+
+var maxProfitLoss = 0
+for(var i=0; i<finances.length; i++){
+  maxProfitLoss = finances[i][1];
+}
+console.log(maxProfitLoss);
